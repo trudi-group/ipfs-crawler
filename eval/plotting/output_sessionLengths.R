20c04e1aa477bf1e7f8972f4d75a76d2c65b8404
Sebastian Henningsen <sebastian.henningsen@hu-berlin.de>
2020-03-24 14:06:18 +0100

    Session length (=uptime) table

---
+++ eval/plotting/output_sessionLengths.R
@@ -0,0 +1,87 @@
+## plot_sessionLengths.R
+
+source("includes.R")
+
+dataPath = "plot_data/session_lengths.csv"
+outTablePath = "tables/session_lengths.tex"
+plotUnits = "mins"
+# plotUnits = "days"
+# plotUnits = "hours"
+cutOff = 0.99
+## We load the average crawl duration from the variables_for_tex.lua file.
+## This dependency is reflected in the Makefile
+minimumCutOff = as.difftime(GetFromEval("avgCrawlDuration"), units="mins")
+units(minimumCutOff) = plotUnits
+minimumCutOff = as.numeric(minimumCutOff)
+
+## Load the data, timedurations are in seconds
+dt = LoadDT(dataPath)
+duration = as.difftime(dt$V2, unit="secs")
+
+totalNumberOfSessions = length(duration)
+writeToEvalRounded("totalNumberOfSessions", totalNumberOfSessions)
+
+## Convert the duration to what we'll use throughout the analysis
+units(duration) = plotUnits
+
+## We will use the ecdf later for a summary table
+empDistr = ecdf(duration)
+
+## Also, compute the cutoff value, everyhing above this percentile
+# will be ignored for the sake of readability in the plot.
+
+cutOffVal = as.numeric(quantile(duration, cutOff))
+
+## Note how many sessions had length 0, i.e., only occured once
+#duration[duration == 0]
+
+## Summarize the counts in a data.table
+duration = as.numeric(duration)
+
+
+### OUTPUT TABLE
+
+## We want to have the number of sessions **longer** than a certain period as well as their
+## percentage in terms of all measured sessions.
+## R's difftime is nice enough to make this very convenient for us
+
+cumulativeValues = c(
+  as.difftime(5, unit="mins"),
+  as.difftime(10, unit="mins"),
+  as.difftime(30, unit="mins"),
+  as.difftime(1, unit="hours"),
+  as.difftime(1, unit="days"),
+  as.difftime(6, unit="days")
+)
+## Fixme: Not the nicest way to duplicate this here, but oh well.
+tableVals = c(
+  "5 minutes",
+  "10 minutes",
+  "30 minutes",
+  "1 hour",
+  "1 day",
+  "6 days"
+)
+units(cumulativeValues) = plotUnits
+
+numSessionsAbove = sapply(cumulativeValues, function(x) {
+  length(duration[duration >= x])
+})
+
+percentageAbove = round(100*numSessionsAbove/totalNumberOfSessions, digits=4)
+
+## Gather them all in a data.table
+
+tableDT = data.table(value=tableVals, percent=percentageAbove, numSessions=numSessionsAbove)
+
+createSessionLengthTable = function(path, d) {
+  fileConn=path
+  cat(c("\\begin{tabular}{| c | c | c |}\n", "\\hline\n", "Session Duration & Percentage & Number of sessions\\\\\n", "\\hline\n"), file=fileConn)
+  
+  for (i in 1:nrow(d)) {
+    cat( c( paste( d[i]$value, d[i]$percent, d[i]$numSessions, sep=" & "), "\\\\\n", "\\hline\n"), append=T, file=fileConn)
+  }
+  cat(c("\\end{tabular}\n"), append=T, file=fileConn)
+}
+
+createSessionLengthTable(outTablePath, tableDT)
---
+++ eval/sessionLengths.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+# Time differences in final .csv file are in **Seconds**
+
+import datetime
+import sys
+import os
+from operator import itemgetter
+import numpy as np
+import time
+
+# Extracts the starting time of a crawl from its filename.
+# Format of the crawlnames is, e.g., visitedPeers_30-11-19--23:54:17_30-11-19--23:58:24.csv
+def extractTimestamp(filename):
+	sname = filename.split("_")
+	startTime = datetime.datetime.strptime(sname[1], "%d-%m-%y--%H:%M:%S")
+	return startTime
+
+# Returns the node ID from a line in the .csv
+def extractIDFromLine(line, offlineEnabled):
+	return(line.split(";")[0] if "true" in line.split(";")[2] or offlineEnabled else None)
+
+def startSession(sdict, nodeid, time):
+	tmpsessions = sdict.get(nodeid, [])
+	tmpsessions.append((time, None))
+	sdict[nodeid] = tmpsessions
+
+def endSession(sdict, nodeid, time):
+	# In this case we are sure that the nodeID is already in the dict. No need for a default value.
+	tmpsessions = sdict[nodeid]
+	tmpsessions[-1] = (tmpsessions[-1][0], time)
+	sdict[nodeid] = tmpsessions
+
+
+if __name__ == "__main__":
+
+	if len(sys.argv) > 1:
+		crawlDir = sys.argv[1]
+	else:
+		crawlDir = "../output_data_crawls/"
+
+	dictOutFile = "plot_data/session_lengths.csv"
+	rawDictOut = "rawDict"
+	offlineEnabled = False
+
+	# Sort the crawls by their startDate. To keep this ordering is crucical for the correctness of the session length calculation
+	crawlFiles = sorted([(fname, extractTimestamp(fname)) for fname in os.listdir(crawlDir) if fname.startswith("visited")],\
+		key=itemgetter(1))
+
+	currentlyOnline = np.array([])
+	# The dict has a list for every node ID that we encounter. The list is a list of tuples with (session start time, session end time)
+	sessionDict = {}
+
+	# ftuple is (filename, crawl time)
+	# We track the sessions until the second to last file, the last file is then just used for "closing" the existing sessions
+	for ftuple in crawlFiles[:-1]:
+		## Crude progress indicator
+		print(ftuple)
+		with open(crawlDir+ftuple[0], "r") as f:
+			# Extract the IDs from the current crawl and remove the None's
+			currentCrawlIDs = [extractIDFromLine(l, offlineEnabled) for l in f]
+			currentCrawlIDs = [nodeid for nodeid in currentCrawlIDs if nodeid]
+			# Add all seen IDs (with their starting time) to the dict, if they are not present.
+			# The idea is to keep track of the currently online IDs for each crawl and update the session dictionary
+			# accordingly.
+
+			# For each ID that was only but is not online now: remove from the set, add the end time of the session to the dict.
+			# For each ID that is online now but is not in the currentlyOnline set, add it to the set and mark the start date
+			# currentCrawlIDs = currentCrawlIDs[0:4]
+
+			# Setdiff1d(a, b): return values in a but not in b.
+			# We need these sets to update our session dictionary, and not to compute the set of currentlyOnline nodes.
+			onlineButNotOnlineBefore = np.setdiff1d(currentCrawlIDs, currentlyOnline)
+			offlineNow = np.setdiff1d(currentlyOnline, currentCrawlIDs)
+
+			for node in onlineButNotOnlineBefore:
+				startSession(sessionDict, node, ftuple[1])
+
+			for node in offlineNow:
+				endSession(sessionDict, node, ftuple[1])
+
+			# Update the nodes that are "currently" online
+			currentlyOnline = currentCrawlIDs
+	
+	# All crawl files have been processed, finish the remaining sessions
+	for node in currentlyOnline:
+		endSession(sessionDict, node, crawlFiles[-1][1])
+	
+	print("Writing dict to file...")
+	## Write the dictionary to file
+	with open(dictOutFile, "w") as f:
+		for entry in sessionDict:
+			for timeTuple in sessionDict[entry]:
+				f.write("%s;%d\n" % (entry, (timeTuple[1]-timeTuple[0]).total_seconds()))
+
+	print("Writing dict with time tuples to file...")
+
+	with open(rawDictOut, "w") as f:
+		for entry in sessionDict:
+			f.write("%s;%s\n" % (entry, sessionDict[entry]))
