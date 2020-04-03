d8c15e542ad6d863698cab390b3927a517353f8d
Sebastian Henningsen <sebastian.henningsen@hu-berlin.de>
2020-04-03 14:21:20 +0200

    Several small tweaks to that the docker eval is more robust

---
+++ run_docker_eval.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+DEFAULTDIR="$(pwd)/output_data_crawls"
+DATADIR=""
+UID=$(id -u $USER)
+
+if [[ $1 == "" ]]; then
+		DATADIR=$DEFAULTDIR
+		echo "No data directory provided, using default $DATADIR"
+else
+	DATADIR=$1
+fi
+
+if [[ ! "$DATADIR" == /* ]]; then
+	echo "Docker needs absolute paths!"
+	exit 1
+fi
+
+echo "Running eval-docker with data directory $DATADIR..."
+sudo docker run \
+	--mount type=bind,source=$DATADIR,target=/output_data_crawls \
+	--mount type=bind,source=$(pwd)/eval/plot_data,target=/eval/plot_data \
+	--mount type=bind,source=$(pwd)/eval/figures,target=/eval/figures \
+	--mount type=bind,source=$(pwd)/eval/tables,target=/eval/tables \
+	-u $UID \
+	scriptkitty/ipfs-crawl-eval
+
+echo "Finished eval, copying report.pdf to $(pwd):"
+cp plot_data/report.pdf $(pwd)/report.pdf
