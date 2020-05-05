a272ddaff11192159c4af3a5cb091377e21291b0
Sebastian Henningsen <sebastian.henningsen@hu-berlin.de>
2020-05-05 19:23:02 +0200

    Parametrize geoIP lookup. It is easy to write a custom resolver now!

--- eval/geoIP_from_crawl.py
+++ eval/geoIP_from_crawl.py
@@ -4,11 +4,96 @@ import os
 import sys
 import geoip2.database
 import ipaddress
-import tempfile
+import json
+import IP2Location
+import abc
 
 from maxminddb import (MODE_AUTO, MODE_MMAP, MODE_MMAP_EXT, MODE_FILE,
                        MODE_MEMORY, MODE_FD)
 
+class GeoIPResolver(abc.ABC):
+
+	## expects an IPv4 or IPv6 address as a string, returns a dictionary with keys:
+	## IP: LocalIP if argument was a local/private IP, else the provided argument
+	## CountryCode: if possible the country code where the IP is from. "NA" for local IPs, "Unkown" in the case of errors.
+	## ASNo: if possible the autonomous system number, "NA" for local IPs or in the case of errors
+	## ASName: if possible the name of the autonomous system organization, "NA" for local IPs or in the case of errors
+	@abc.abstractmethod
+	def resolveIP(self, ip):
+		pass
+
+class MaxMindResolver(GeoIPResolver):
+
+	def __init__(self, dbPath, countryDBFile = "GeoLite2-Country.mmdb", ASDBFile = "GeoLite2-ASN.mmdb"):
+		self.dbPath = dbPath
+		self.countryDBFile = countryDBFile
+		self.ASDBFile = ASDBFile
+		self.countryDB = geoip2.database.Reader(os.path.join(self.dbPath, self.countryDBFile))#, mode=MODE_MMAP_EXT)
+		self.ASDB = geoip2.database.Reader(os.path.join(self.dbPath, self.ASDBFile))#, mode=MODE_MMAP_EXT)
+
+	def resolveIP(self, ip):
+		# print(ip)
+		tmpDict = {}
+		if ipaddress.ip_address(ip).is_private or ipaddress.ip_address(ip).is_link_local:
+			tmpDict["IP"] = "LocalIP"
+			tmpDict["CountryCode"] = "NA"
+			tmpDict["ASNo"] = "NA"
+			tmpDict["ASName"] = "NA"
+		else:
+			try:
+				countryc = self.countryDB.country(ip).country.iso_code
+
+				if countryc == None:
+					countryc = "Unknown"
+
+				resp = self.ASDB.asn(ip)
+				tmpDict["IP"] = ip
+				tmpDict["CountryCode"] = countryc
+				tmpDict["ASNo"] = resp.autonomous_system_number
+				tmpDict["ASName"] = resp.autonomous_system_organization
+
+			except geoip2.errors.AddressNotFoundError:
+				# Just append "Unknown"
+				tmpDict["IP"] = ip
+				tmpDict["CountryCode"] = "Unknown"
+				tmpDict["ASNo"] = "NA"
+				tmpDict["ASName"] = "NA"
+
+		return tmpDict
+
+
+class IP2LocationResolver(GeoIPResolver):
+
+	def __init__(self, dbPath):
+		print("Warning: Autonomous system lookup not implemented yet for IP2Location!")
+		self.dbPath = dbPath
+		self.IPv4Resolver = IP2Location.IP2Location(os.path.join(self.dbPath, "IP2LOCATION-LITE-DB1.BIN"), "SHARED_MEMORY")
+		self.IPv6Resolver = IP2Location.IP2Location(os.path.join(self.dbPath, "IP2LOCATION-LITE-DB1.IPV6.BIN"), "SHARED_MEMORY")
+
+	def resolveIP(self, ip):
+		tmpDict = {}
+		if ipaddress.ip_address(ip).is_private or ipaddress.ip_address(ip).is_link_local:
+			tmpDict["IP"] = "LocalIP"
+			tmpDict["CountryCode"] = "NA"
+			tmpDict["ASNo"] = "NA"
+			tmpDict["ASName"] = "NA"
+		else:
+			resolv = None
+			if ipaddress.ip_address(ip).version == 4:
+				resolv = self.IPv4Resolver
+			else:
+				resolv = self.IPv6Resolver
+
+			resp = resolv.get_all(ip)
+			tmpDict["IP"] = ip
+			tmpDict["CountryCode"] = resp.country_short
+			tmpDict["ASNo"] = "NA"
+			tmpDict["ASName"] = "NA"
+
+		return tmpDict
+
+
+#### Helper Functions ####
 
 def ParseMA(maString):
 	# The format of such an address is
@@ -33,6 +118,9 @@ def extractTimestamp(filename):
 	# Example for a filename: visitedPeers_29-11-19--10:30:25_29-11-19--10:41:29.csv
 	return filename.split("_")[1]
 
+
+
+
 ############# MAIN ############
 
 if __name__ == "__main__":
@@ -50,11 +138,10 @@ if __name__ == "__main__":
 	peerFiles = [x for x in allFiles if x.startswith("visited")]
 	processedFiles = os.listdir(outputDir)
 	
-	onlyOnlineNodes = False
-	geoIPPathCountry = "geoipDBs/GeoLite2-Country.mmdb"
-	geoIPPathAS = "geoipDBs/GeoLite2-ASN.mmdb"
-	geoIPDB = geoip2.database.Reader(geoIPPathCountry)#, mode=MODE_MMAP_EXT)
-	ASDB = geoip2.database.Reader(geoIPPathAS)#, mode=MODE_MMAP_EXT)
+	# onlyOnlineNodes = False
+	geoIPPath = "geoipDBs/"
+	resolver = MaxMindResolver(geoIPPath)
+	# resolver = IP2LocationResolver(geoIPPath)
 
 	i = 0
 	for crawlFile in peerFiles:
@@ -67,56 +154,38 @@ if __name__ == "__main__":
 		print(crawlFile)
 		print("Progress: " + str(i*100/len(peerFiles)) + "%")
 		totalCountryCount = 0
-		tmpf = tempfile.NamedTemporaryFile(mode="r+", encoding="utf-8")
-		tmpf.write("nodeid;IP;country;ASNO;ASName;online;agentVersion\n")
-		with open(crawlDir+crawlFile, "r") as f:
-			lines = [l for l in f]
 
-			for l in lines:
+		crawldata = {}
+
+		with open(crawlDir+crawlFile, "r") as f:
+			crawldata = json.load(f)
+
+			for node in crawldata["Nodes"]:
 				# Roadmap: 
-				# * Take each line and split it into its three components
-				# * Lookup the country code, AS-no and AS name for each IP
+				# * Take each entry and lookup the country code, AS-no and AS name for each IP
 				# * Put everything back together
 				# * Write back to *the same* file (through a temp-file)
-				s = l.split(";")
-				nodeid = s[0]
-				nodeOnline = s[2]
-				agentVersion = s[3]
-				# Strip the brackets around the MA list, split at the whitespaces, extract the IP (if possible) and ignore 'None' addresses
-				rawMA = s[1].strip("[]")
+
+				multiaddrs = node["MultiAddrs"]
 				# We had at least one occurence of an ID without address -> ignore that
-				if len(rawMA) == 0:
-					print("No addresses to peer ID " + nodeid)
+				if len(multiaddrs) == 0:
+					print("No addresses to peer ID " + node["NodeID"])
 					continue
 
-				rawMA = rawMA.split(" ")
-				MAProtArray = [ParseMA(ma) for ma in rawMA]
+				## TODO: Properly split the addresses. libp2p-py seems a bit outdated and unstable, but maybe it can be of value?
+				MAProtArray = [ParseMA(ma) for ma in multiaddrs]
 				# We get (protocol, addr) back, if the address is ipv4 or ipv6
 				AddrArray = list(set([ma[1] for ma in MAProtArray if ma[1]]))
 
-				# out format for the output is nodeID;[<ip, country_code, as_no, as_name>, <...>, ...];true/false
+				# We now proceed with the lookup and enhance the simple list of multiaddrs by more detailed info:
+				tmpAddrLst = []
 				for ip in AddrArray:
-					if ipaddress.ip_address(ip).is_private or ipaddress.ip_address(ip).is_link_local:
-						addrs = str(ip) + ";LocalIP;NA;NA"
-					else:
-						try:
-							countryc = geoIPDB.country(ip).country.iso_code
-							if countryc == None:
-								countryc = "Unknown"
+					tmpDict = resolver.resolveIP(ip)
+					tmpAddrLst.append(tmpDict)
 
-							resp = ASDB.asn(ip)
-							addrs = str(ip) + ";" + str(countryc) + ";" + str(resp.autonomous_system_number) +\
-							";" + str(resp.autonomous_system_organization)
+				node["MultiAddrs"] = tmpAddrLst
+				# print(crawldata["Nodes"][0])
 
-						except geoip2.errors.AddressNotFoundError:
-							# Just append "Unknown"
-							addrs = str(ip) + ";Unknown;NA;NA"
-					# This code assumes the updated output format with 4 columns, the 4th being the agent version
-					tmpf.write(nodeid + ";" + addrs + ";" + nodeOnline + ";" + agentVersion)
-
-		tmpf.seek(0)
 		with open(outputDir+crawlFile, "w", encoding="utf-8") as f:
-			for l in tmpf:
-				f.write(l)
-		tmpf.close()
+			json.dump(crawldata, f)
 		i += 1
